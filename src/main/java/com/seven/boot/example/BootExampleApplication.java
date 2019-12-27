b0c4b609aeaad3e04b581480738e4f2d76c5227b
HaoQiao <h_qiao1126@163.com>
2019-12-27 11:19:52 +0800

    init

---
+++ src/main/java/com/seven/boot/example/BootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.seven.boot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootExampleApplication.class, args);
+    }
+
+}
